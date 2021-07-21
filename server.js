1f764cb560c9db5ec9f13632e424cb8ce5446117
Nagendra Yanamadala <nyanamadala@dstworldwideservices.com>
2021-07-21 19:57:24 +0530

    port changed

--- server.js
+++ server.js
@@ -81,8 +81,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     // ========================
     const isProduction = process.env.NODE_ENV === 'production'
     const port = isProduction ? 7500 : 3000
-    app.listen(8080, function () {
-      console.log(`listening on ${8080}`)
+    app.listen(process.env.PORT || 5000, '0.0.0.0', function () {
+      console.log(`listening on ${process.env.PORT}`)
     })
   })
   .catch(console.error)
